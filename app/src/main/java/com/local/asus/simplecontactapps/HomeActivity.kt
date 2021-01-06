206e1f1b623c41d17620f8255449ddec24923907
yona than <yonabaho@gmail.com>
2021-01-06 17:01:11 +0700

    Added menu popup

--- app/src/main/java/com/local/asus/simplecontactapps/HomeActivity.kt
+++ app/src/main/java/com/local/asus/simplecontactapps/HomeActivity.kt
@@ -1,6 +1,5 @@
 package com.local.asus.simplecontactapps
 
-
 import android.content.DialogInterface
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
@@ -15,17 +14,19 @@ import kotlinx.android.synthetic.main.home_activity.*
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.alert_dialog.view.*
-import java.lang.Exception
+import kotlinx.android.synthetic.main.kontak_mini_menu.view.*
 
 class HomeActivity : AppCompatActivity(), KontakRecyclerAdapter.ItemListener {
 
-    lateinit var alertDialog: AlertDialog.Builder
+    lateinit var miniAlert: AlertDialog.Builder
+    lateinit var deleteAlert: AlertDialog.Builder
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.home_activity)
 
-        alertDialog = AlertDialog.Builder(this)
+        deleteAlert = AlertDialog.Builder(this)
+        miniAlert = AlertDialog.Builder(this)
 
         add_contact.setOnClickListener {
             startActivity(Intent(applicationContext, AddContactActivity::class.java))
@@ -41,28 +42,72 @@ class HomeActivity : AppCompatActivity(), KontakRecyclerAdapter.ItemListener {
 
     }
 
-    override fun onItemClick(kontak: Kontak) {
+    fun startDetailActivity(kontak: Kontak){
         intent = Intent(applicationContext, DetailContactActivity::class.java)
         intent.putExtra("kontak", kontak)
         startActivity(intent)
     }
 
+    override fun onItemClick(kontak: Kontak) {
+        startDetailActivity(kontak)
+    }
+
     override fun onLongItemClick(kontak: Kontak) {
-        var layout = layoutInflater.inflate(R.layout.alert_dialog, null)
-        alertDialog.setView(layout)
-        var dialog = alertDialog.create()
-        layout.alert_cancel.setOnClickListener {
-            Toast.makeText(applicationContext, "Batal hapus", Toast.LENGTH_LONG).show()
-            dialog.dismiss()
-        }
-        layout.alert_confirm.setOnClickListener {
-            Toast.makeText(applicationContext, "Hapus", Toast.LENGTH_LONG).show()
-            dialog.dismiss()
+        var miniLayout = layoutInflater.inflate(R.layout.kontak_mini_menu, null)
+        miniLayout.mini_menu_nama.text = kontak.nama
+        miniLayout.mini_menu_nomor.text = kontak.phone
+
+
+        miniAlert.setView(miniLayout)
+        var dialog2 = miniAlert.create()
+
+        miniLayout.mini_menu_lihat.setOnClickListener {
+            startDetailActivity(kontak)
+            dialog2.dismiss()
         }
 
-        if (dialog.window != null)
-            dialog.window!!.getAttributes().windowAnimations = R.style.SlidingDialogAnimation
+        miniLayout.mini_menu_hapus.setOnClickListener {
+            dialog2.dismiss()
+            var layout = layoutInflater.inflate(R.layout.alert_dialog, null)
+            deleteAlert.setView(layout)
+            var dialog = deleteAlert.create()
+            layout.alert_cancel.setOnClickListener {
+                Toast.makeText(applicationContext, "Batal hapus", Toast.LENGTH_LONG).show()
+                dialog.dismiss()
+            }
+            layout.alert_confirm.setOnClickListener {
+                Toast.makeText(applicationContext, "Hapus", Toast.LENGTH_LONG).show()
+                dialog.dismiss()
+            }
 
-        dialog.show()
+            if (dialog.window != null)
+                dialog.window!!.getAttributes().windowAnimations = R.style.SlidingDialogAnimation
+
+            dialog.show()
+        }
+
+        if (dialog2.window != null)
+            dialog2.window!!.getAttributes().windowAnimations = R.style.MiniMenuAnimation
+
+        dialog2.show()
+        dialog2.window!!.setLayout(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT)
+
+
+//        var layout = layoutInflater.inflate(R.layout.alert_dialog, null)
+//        deleteAlert.setView(layout)
+//        var dialog = deleteAlert.create()
+//        layout.alert_cancel.setOnClickListener {
+//            Toast.makeText(applicationContext, "Batal hapus", Toast.LENGTH_LONG).show()
+//            dialog.dismiss()
+//        }
+//        layout.alert_confirm.setOnClickListener {
+//            Toast.makeText(applicationContext, "Hapus", Toast.LENGTH_LONG).show()
+//            dialog.dismiss()
+//        }
+
+//        if (dialog.window != null)
+//            dialog.window!!.getAttributes().windowAnimations = R.style.SlidingDialogAnimation
+//
+//        dialog.show()
     }
 }
